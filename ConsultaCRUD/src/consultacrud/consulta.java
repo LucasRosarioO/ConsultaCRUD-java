89719f0ec6584a69d43b4ce583aa80866e05c5f7
Lucas Rosario <lucas.rosario25@gmail.com>
2020-04-11 03:28:11 -0300

    Add files via upload

---
+++ ConsultaCRUD/src/consultacrud/consulta.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consultacrud;
+
+import apresentacao.JFrameConsulta;
+
+/**
+ *
+ * @author Lucas Rosario de Oliveira - 2017101742
+ */
+public class consulta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrameConsulta JFrameConsulta = new JFrameConsulta(); //Cria o objeto Janela
+        JFrameConsulta.setTitle("Cadastro de Consultas"); //Define o título na Janela
+        JFrameConsulta.setLocationRelativeTo(null); //Coloca a janela no centro da tela
+        JFrameConsulta.setVisible(true); //Mostra a janela
+
+        
+
+    }
+    
+}
